c128007b8004a16e62e97d8ca228c0463075581e
Melvillian <melvillevt@gmail.com>
2024-10-06 16:19:00 -0400

    WIP: making sure there aren't any duplicates

--- src/core/helpers.rs
+++ src/core/helpers.rs
@@ -1,25 +1,83 @@
-use dendron::{Node, Tree};
-
 use super::datatypes::Block;
+use dendron::{traverse::DftEvent, Tree};
 
+fn build_markdown_from_tree(tree: Tree<Block>, markdown: &mut String) {
+    let mut depth = 0;
+
+    for evt in tree.root().depth_first_traverse() {
+        match &evt {
+            DftEvent::Open(_) => {
+                depth += 1;
+            }
+            DftEvent::Close(_) => {
+                depth -= 1;
+            }
+        }
+        let block = evt.as_value().borrow_data();
+        println!("{}", serde_json::to_value(block.clone()).unwrap());
+        let tabs = "\t".repeat(depth);
+        markdown.push_str(&format!("{}{}\n", tabs, block.to_markdown()));
+    }
+    assert!(depth == 0);
+}
+/// Builds a markdown representation for each Tree in trees by traversing through each
+/// tree using DFS (depth first search). The depth of the tree is represented as a number of
+/// tabs in front of the line, and each line is a new Block in the Tree
+///
+/// # Examples
+///
+/// ```
+/// let root1 = tree_node! {
+/// Block { text: "Watch General Magic"}, [
+///     Block { text: "It's a good documentary"},
+///     Block { text: "it's a positive story about technology"},
+///     Block { text: "it shows engineer trying to build cool stuff"}, [
+///         Block { text: "such as phones"},,
+///     ]),
+/// ]};
+/// let root2 = tree_node! {
+/// Block { text: "Cook Dinner"}, [
+///     Block { text: "Buy ingredients"},
+///     Block { text: "cook them, mash them, stick em in a stew"},
+/// ]};
+///
+/// let markdown = build_markdown_from_trees(vec![root1, root2]);
+///
+/// assert_eq!(markdown,
+/// "Watch General Magic
+///     It's a good documentary
+///     it's a positive story about technology
+///     it shows engineer trying to build cool stuff
+///         such as phones
+/// Cook Dinner
+///     Buy ingredients
+///     cook them, mash them, stick em in a stew
+/// ");
+///
 pub fn build_markdown_from_trees(trees: Vec<Tree<Block>>) -> String {
     let mut markdown = String::new();
 
     for tree in trees {
-        build_markdown_recursive(tree.root(), 0, &mut markdown);
+        build_markdown_from_tree(tree, &mut markdown)
     }
 
     markdown
 }
 
-fn build_markdown_recursive(node: Node<Block>, depth: usize, markdown: &mut String) {
-    let tabs = "\t".repeat(depth);
-    markdown.push_str(&format!("{}{}\n", tabs, node.borrow_data().to_markdown()));
+#[cfg(test)]
+mod tests {
+    use dendron::{tree_node, Node};
 
-    // println!("{}", &format!("{}{}\n", tabs, node.borrow_data().text));
-    // println!("{}", node.)
+    use super::*;
 
-    for child in node.children() {
-        build_markdown_recursive(child, depth + 1, markdown);
+    fn fake_tree_for_markdown_building() -> Vec<Tree<Block>> {
+        let root1: Tree<Block> = (tree_node! {
+          serde_json::from_str(r#"{"block_type":{"paragraph":{"color":"default","rich_text":[{"annotations":{"bold":false,"code":false,"color":"default","italic":false,"strikethrough":false,"underline":false},"plain_text":"11:14: Plan For day:","text":{"content":"11:14: Plan For day:"},"type":"text"}]},"type":"paragraph"},"creation_date":"2024-10-05T15:14:00Z","has_children":true,"id":"1164f233-166c-8100-a937-f753bc111dba","page_id":"1164f233-166c-80f1-88d0-c68546042265","parent_block_id":null,"text":"11:14: Plan For day:","update_date":"2024-10-06T18:51:00Z"}"#).unwrap()
+        }).tree();
+
+        vec![root1]
     }
+
+    #[test]
+    fn test_build_markdown_from_trees() {}
 }
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,15 @@
 use chrono::{Duration, Utc};
 use dotenv::dotenv;
-use dross::{
-    core::{datatypes::Block, helpers::build_markdown_from_trees},
-    notion::Notion,
-};
+use dross::{core::helpers::build_markdown_from_trees, notion::Notion};
 use log::{debug, info, trace};
-use std::{collections::HashSet, env};
+use std::env;
 
 #[tokio::main]
 async fn main() {
     dotenv().ok();
     env_logger::init();
 
-    let dur: Duration = Duration::days(match env::var("RUST_LOG") {
+    let dur: Duration = Duration::hours(match env::var("RUST_LOG") {
         Ok(log_level) => match log_level.to_lowercase().as_str() {
             "debug" | "trace" => 1,
             _ => 7,
@@ -30,6 +27,8 @@ async fn main() {
     let pages_edited_after_cutoff_date = notion.get_last_edited_pages(cutoff).await.unwrap();
     info!(target: "notion", "retrieved {} Pages edited in the last {} days", pages_edited_after_cutoff_date.len(), dur.num_days());
     let mut pages_and_block_roots = Vec::new();
+    // TODO: instead of storing the whole Block data, which is 95% worthless data, just strip out the
+    // text and id, store that in a struct, and use that to build the markdown
     for page in pages_edited_after_cutoff_date {
         debug!(target: "notion", "Page URL: {}", page.url);
 
@@ -47,19 +46,6 @@ async fn main() {
         info!(target: "notion", "expanding {} block roots for page: {}", block_roots.len(), page.title);
         trace!(target: "notion", "the block roots look like: {:?}", block_roots.iter().map(|b| (&b.text, &b.id, &b.block_type)).collect::<Vec<_>>());
         let trees = notion.expand_block_roots(block_roots).await.unwrap();
-
-        let mut duplicate_checker: HashSet<Block> = HashSet::new();
-        for tree in &trees {
-            let prohibition = tree.prohibit_hierarchy_edit().unwrap();
-            let root_frozen_node = tree.root().bundle_hierarchy_edit_prohibition(&prohibition);
-            for (frozen_node, _depth) in
-                root_frozen_node.allocating_breadth_first_traverse_stable(0)
-            {
-                if !duplicate_checker.insert(frozen_node.borrow_data().clone()) {
-                    info!(target: "notion", "Found duplicate block: {:?}", frozen_node.borrow_data());
-                }
-            }
-        }
 
         let single_page_prompt_markdown = build_markdown_from_trees(trees);
         every_prompt_markdown.push(format!(
